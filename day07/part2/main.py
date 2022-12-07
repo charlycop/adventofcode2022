478adbcc4c76980f55534dd688cfa63be1b00bf2
Charly Oudy <charly.oudy@lip6.fr>
2022-12-07 15:16:10 +0100

    ajout de ascii art day7 part 2

--- day07/part2/main.py
+++ day07/part2/main.py
@@ -1,28 +1,137 @@
-# declare constants 
-MARKER_LENGTH = 14
+import sys # for sys.maxsize
+"""
+   _____                _              _       
+  / ____|              | |            | |      
+ | |     ___  _ __  ___| |_ __ _ _ __ | |_ ___ 
+ | |    / _ \| '_ \/ __| __/ _` | '_ \| __/ __|
+ | |___| (_) | | | \__ \ || (_| | | | | |_\__ \
+  \_____\___/|_| |_|___/\__\__,_|_| |_|\__|___/
+"""
+FILENAME        = 'adventofcode/day07/inputs.txt'
+ROOT            = 0
+CD_CMD_POSITION = 1
+CD_ARG_POSITION = 2
+CD_COMMAND      = 'cd'
+CD_ARG_ROOT     = '/'
+CD_ARG_DOWN     = '..'
+SIZE_POSITION   = 0
+THRESHOLD       = 100000
+CURRENT         = -1
+SIZE            = 1
+NAME            = 0
+SIZE_ZERO       = 0
+TOTAL_MEMORY    = 70000000
+UNUSED_SPACE_TH = 30000000
 
-filename = 'adventofcode/day07/inputs.txt'
+"""
+   _____ _       _           _     
+  / ____| |     | |         | |    
+ | |  __| | ___ | |__   __ _| |___ 
+ | | |_ | |/ _ \| '_ \ / _` | / __|
+ | |__| | | (_) | |_) | (_| | \__ \
+  \_____|_|\___/|_.__/ \__,_|_|___/
+"""                                                
 datas = []
+directories_dict     = {}
+level_counter        = ROOT
+folders_depth        = []
+total_size           = 0
+somme_size           = 0
+numbers_of_folder    = 0
 
-with open(filename, 'r') as handler:
+"""
+  ______                _   _                 
+ |  ____|              | | (_)                
+ | |__ _   _ _ __   ___| |_ _  ___  _ __  ___ 
+ |  __| | | | '_ \ / __| __| |/ _ \| '_ \/ __|
+ | |  | |_| | | | | (__| |_| | (_) | | | \__ \
+ |_|   \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
+"""
+def is_a_size(line):
+    size_str = line[0]
+    return (ord(size_str[0]) > 47 and ord(size_str[0]) < 58)
+
+# add a suffix to the folder name in case it's already in the dictionnary
+def rename_dir_if_needed(folder):
+    
+    if (directories_dict.get(folder[NAME]) != None):
+        try:   rename_dir_if_needed.counter += 1
+        except AttributeError: rename_dir_if_needed.counter = 0
+        folder[NAME] += '_' + str(rename_dir_if_needed.counter)
+
+    return folder
+
+def add_to_dictionnary(folder):
+    child_folder = rename_dir_if_needed(folder)
+    directories_dict[child_folder[NAME]] = child_folder[SIZE]      #record it into the final dictionnary
+    folders_depth[CURRENT][SIZE] += child_folder[SIZE]             #add the size to the parent directory
+
+def get_quickly_total_size(datas):
+    quick_total_size = 0
+    for line in datas:
+        line = line.split()
+        if is_a_size(line):
+            quick_total_size += int(line[SIZE_POSITION])
+    
+    return quick_total_size
+
+def get_quickly_unused_space(used_size):
+    return TOTAL_MEMORY - used_size
+
+""" 
+  _____                                     
+ |  __ \                                    
+ | |__) | __ ___   __ _ _ __ __ _ _ __ ___  
+ |  ___/ '__/ _ \ / _` | '__/ _` | '_ ` _ \ 
+ | |   | | | (_) | (_| | | | (_| | | | | | |
+ |_|   |_|  \___/ \__, |_|  \__,_|_| |_| |_|
+                   __/ |                    
+                  |___/   
+"""
+#Getting the datas from the input file
+with open(FILENAME, 'r') as handler:
     datas = handler.readlines()
-    datas = datas[0]
 
-marker_found = True
+for line in datas:
+    line = line.split()
 
-for i in range(MARKER_LENGTH, len(datas)):
-    message_marker = datas[i-MARKER_LENGTH:i]
+    if line[CD_CMD_POSITION] == CD_COMMAND:
+        dest = line[CD_ARG_POSITION]
+        
+        if   dest == CD_ARG_ROOT : # initialisation
+            level_counter = ROOT
+            folders_depth.append([CD_ARG_ROOT, SIZE_ZERO]) 
+
+        elif dest == CD_ARG_DOWN :
+            level_counter -= 1
+            add_to_dictionnary(folders_depth.pop())
+        
+        else : 
+            level_counter += 1
+            folders_depth.append([dest, SIZE_ZERO])
     
-    for letter in message_marker:
-        if message_marker.count(letter) > 1 : 
-            marker_found = False
-            break
+    elif is_a_size(line):
+        size = int(line[SIZE_POSITION])
+        folders_depth[CURRENT][SIZE] += size
+        total_size                   += size
+
+#to do the last one
+add_to_dictionnary(folders_depth.pop())
+
+unused_space = get_quickly_unused_space(total_size)
+space_needed = UNUSED_SPACE_TH - unused_space
+
+size_dir_to_delete = sys.maxsize
+
+for dir_size in directories_dict.values():
     
-    if marker_found : break
-    else : marker_found = True
+    if space_needed == dir_size :
+        size_dir_to_delete = dir_size
+        break
+    
+    if space_needed < dir_size and dir_size < size_dir_to_delete :
+        size_dir_to_delete = dir_size
 
-if marker_found:
-    print("First marker after character : ", i)
-else:
-    print("No Marker Found !")
-
+print("The total size is : ", total_size)
+print("Unused space =", unused_space, "- Space needed :", space_needed)
+print("Size of the directory to delete = ", size_dir_to_delete)
